7e8593d9f60c53e0be3b6f7a5d4111aa8dd0b214
lokeshagg13 <lkaggarwal1997@gmail.com>
2025-06-26 22:56:29 +0530

    [BUILD]

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-black text-white mt-4 p-4 w-full">
+    <footer className="bg-black text-white mt-4 p-4 w-full absolute bottom-0">
       <div className="text-center w-full">
         <div className="text-sm text-center text-gray">
           Designed and Developed by
