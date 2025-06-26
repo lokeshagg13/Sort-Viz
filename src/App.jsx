58b5a3552a5fb7ec4ebe2653c61022f45f76f0c5
lokeshagg13 <lkaggarwal1997@gmail.com>
2025-06-26 22:53:01 +0530

    [REFACTOR] - Add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import Footer from "./components/Footer";
 import MainPanel from "./components/MainPanel";
 import HeaderImage from "./images/header.png";
 
 function App() {
   return (
-    <div className="app p-4 bg-black min-h-screen">
-      <div className="flex items-center justify-center mb-4">
+    <div className="app bg-black min-h-screen">
+      <div className="flex p-4 items-center justify-center mb-4">
         <img src={HeaderImage} alt="Sort Viz" width={500} />
       </div>
       <MainPanel />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <footer className="bg-black text-white mt-4 p-4 w-full">
+      <div className="text-center w-full">
+        <div className="text-sm text-center text-gray">
+          Designed and Developed by
+          <span className="text-purple"> Lokesh Aggarwal</span>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
